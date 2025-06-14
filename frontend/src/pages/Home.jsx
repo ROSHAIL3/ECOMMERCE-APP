d775c60bf0658b6f19eed706f764b0a014d04f6c
rtanvir <tfroshail@gmail.com>
2025-06-14 14:27:00 +0300

    header

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../components/Hero';
 
-const home = () => {
+const Home = () => {
   return (
-    <div>home</div>
-  )
-}
+    <div>
+      <Hero />
+    </div>
+  );
+};
 
-export default home
+export default Home;
